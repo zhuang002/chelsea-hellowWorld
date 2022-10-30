74584ec3f27ec6fbfa1713850db394c51f9f2779
zhuang002 <zhuang001@gmail.com>
2022-10-30 11:47:17 -0400

    add setsample

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	
@@ -24,7 +26,59 @@ public class Main {
 		//arraySample();
 		//bitOperationSample();
 		//stringSample();
-		arrayListSample();
+		//arrayListSample();
+		setSample();
+		
+		
+	}
+
+	private static void setSample() {
+		// TODO Auto-generated method stub
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> set2 = new HashSet<>();
+		
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+
+		set1.add(3);
+		System.out.println(set1);
+		
+		set2.add(2);
+		set2.add(3);
+		set2.add(4);
+		set2.add(5);
+		
+		System.out.println(set2);
+		
+		set1.retainAll(set2);
+		System.out.println(set1);
+		
+		set1.clear();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		
+		set1.addAll(set2);
+		System.out.println(set1);
+		
+		set1.clear();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		
+		set1.removeAll(set2);
+		System.out.println(set1);
+		
+		set1.clear();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		
+		
+		set2.removeAll(set1);
+		System.out.println(set2);
+		
 		
 		
 	}
